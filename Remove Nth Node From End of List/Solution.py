c474065ea89253cb4ba6c7693c1791a3b7550367
nader <nader@192.168.1.16>
2021-01-01 00:39:40 +0200

    [feat] Remove Nth Node From End of List

---
+++ Remove Nth Node From End of List/Solution.py
@@ -0,0 +1,18 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        size = 1
+        cur = p = head
+        while cur.next:
+            size += 1
+            cur = cur.next
+            if size > n + 1:
+                p = p.next
+        if size == n:
+            return head.next
+        else:
+            p.next = p.next.next
+            return head
