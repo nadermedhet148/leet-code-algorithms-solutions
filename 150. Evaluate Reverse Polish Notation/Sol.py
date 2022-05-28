7dee4583d7292507c2af58199e337213175bf836
nadermedhet148 <nadermedhet142@gmail.com>
2022-05-28 21:29:55 +0200

    150. Evaluate Reverse Polish Notation

---
+++ 150. Evaluate Reverse Polish Notation/Sol.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = []
+        for t in tokens:
+            if t not in "+-*/":
+                stack.append(int(t))
+            else:
+                r, l = stack.pop(), stack.pop()
+                if t == "+":
+                    stack.append(l+r)
+                elif t == "-":
+                    stack.append(l-r)
+                elif t == "*":
+                    stack.append(l*r)
+                else:
+                    stack.append(int(float(l)/r))
+        return stack.pop()
+
+
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+s = Solution().evalRPN(tokens);
