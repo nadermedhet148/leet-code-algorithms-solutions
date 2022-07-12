fec86606905f6069f9100222e841a47daa89bea3
nadermedhet148 <nadermedhet142@gmail.com>
2022-07-12 11:16:20 +0200

    21 . mergeTwoLists

--- 21. Merge Two Sorted Lists/sol.py
+++ 21. Merge Two Sorted Lists/sol.py
@@ -1,19 +1,20 @@
 # Definition for singly-linked list.
-from soupsieve import select
 
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+
 class Solution:
     def mergeTwoLists(self, a, b):
         if not a or b and a.val > b.val:
-            a,b = b,a
-        if a :
-            a = self.mergeTwoLists(a.next , b)
-        return
-        
+            a, b = b, a
+        if a:
+            a.next = self.mergeTwoLists(a.next, b)
+        return a
         
 
 
@@ -27,3 +28,5 @@ ssecond = ListNode(2,sthird)
 sfirst = ListNode(1,ssecond)
 
 s = Solution().mergeTwoLists(ofirst,sfirst)
+
+print(s)
